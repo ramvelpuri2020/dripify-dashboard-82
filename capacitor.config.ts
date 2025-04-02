65b20040a78ad3d83fe0904965ae89a1457e49fc
Rohini <vroh2021@gmail.com>
2025-04-02 17:25:55 -0400

    Sync for Appflow

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,21 +1,9 @@
-
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.7c4d7fc5c8014246b1f359518b26b0e8',
-  appName: 'GenStyle',
-  webDir: 'dist',
-  server: {
-    url: 'https://7c4d7fc5-c801-4246-b1f3-59518b26b0e8.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
-  ios: {
-    contentInset: 'automatic',
-    limitsNavigationsToAppBoundDomains: true,
-  },
-  android: {
-    allowMixedContent: true
-  }
+  appId: 'com.dripcheck.app',
+  appName: 'DripCheck',
+  webDir: 'dist'
 };
 
 export default config;
